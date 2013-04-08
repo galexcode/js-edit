d27f1190705634c782b409b6ebbcf509f4d06d12
Jason Held <jsonheld@gmail.com>
2013-04-08 18:23:33 -0400

    can bold/unbold whole editor content...not handling selection ranges yet.

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ $(document).ready(function() {
     if ( editor_div_string.length == 0 ) {
 	$('#editor_div').text('This is standard loaded text.');
     }
+    $('#bold_button').click(function() {
+	var range = window.getSelection().getRangeAt(0);
+	// bold/unbold only the selected text range
+	if ( range.toString().length ) {
+	    alert('only handles when nothing is selected right now, sorry!');
+	}
+	// If no selection do all (just for dev purposes).
+	else {
+	    var font_weight = 'bold';
+	    if ( $('#editor_div').css('font-weight') > 400 ) {
+		font_weight = 'normal';
+	    }
+	    $('#editor_div').css('font-weight',font_weight);
+	}
+    })
 });
