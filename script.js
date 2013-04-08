1e127650e86ff42d57996003f09a0dc4d81313ca
Jason Held <jsonheld@gmail.com>
2013-04-08 17:39:55 -0400

    only load temp text if no content.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $('#editor_div').text('This is standard loaded text.');
+    
+    // Temp text to load if nothing typed in. Remove after initial development.
+    var editor_div_string = $('#editor_div').text().replace(/^\s+|\s+$/g,"");
+    if ( editor_div_string.length == 0 ) {
+	$('#editor_div').text('This is standard loaded text.');
+    }
 });
